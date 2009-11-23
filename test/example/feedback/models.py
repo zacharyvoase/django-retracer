383f0ce419984ab2a9935b6b40c78af48ffe8b37
Zachary Voase <zacharyvoase@me.com>
2009-11-23 11:39:09 +0100

    Created the Feedback model in the example app.

--- test/example/feedback/models.py
+++ test/example/feedback/models.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Feedback(models.Model):
+    
+    name = models.CharField(max_length=50)
+    text = models.TextField(blank=False)
+    created_at = models.DateTimeField(default=datetime.datetime.utcnow, editable=False)
