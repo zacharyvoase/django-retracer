b33aa5e4b51fe9f097c572ab318d787ec080f0c1
Zachary Voase <zacharyvoase@me.com>
2009-11-23 12:37:34 +0100

    Refactored some code and added nonce support to unstashing.

--- src/djretr/middleware.py
+++ src/djretr/middleware.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import random
+import string
+import urllib
+import urlparse
+
 from django import http
 from django.conf import settings
 from django.core.urlresolvers import reverse
+
+
+def make_nonce(length=10, chars=(string.letters + string.digits)):
+    """Generate a random nonce (number used once)."""
+    
+    return ''.join(random.choice(chars) for i in xrange(length))
+
+
+def add_query_param(url, param, value):
+    """Add a query parameter to a URL."""
+    
+    split = list(urlparse.urlparse(url))
+    if split[4]:
+        split[4] += '&'
+    split[4] += urllib.urlencode([(param, value)])
+    return urlparse.urlunparse(split)
 
 
 class RetracerRequestMixin(object):
@@ -39,22 +60,25 @@ class RetracerRequestMixin(object):
             return True
         return False
     
-    def unstash_location(self, permanent=False):
+    def unstash_location(self, nonce=False, permanent=False):
         location = self.pop_location()
+        if nonce:
+            location = add_query_param(location, make_nonce(), '')
         if permanent:
             return http.HttpResponsePermanentRedirect(location)
         return http.HttpResponseRedirect(location)
     
-    def unstash_location_with_default(self, view_name, permanent=False, *args, **kwargs):
-        if view_name.startswith('/'):
+    def unstash_location_with_default(self, view_name, args=None, kwargs=None,
+                                      nonce=False, permanent=False):
+        if '/' in view_name:
             default = view_name
         else:
             default = reverse(view_name, args=args, kwargs=kwargs)
         
-        location = self.pop_location(default=default)
-        if permanent:
-            return http.HttpResponsePermanentRedirect(location)
-        return http.HttpResponseRedirect(location)
+        if self.get_location() is None:
+            self.stash_location(default)
+        
+        return self.unstash_location(nonce=nonce, permanent=permanent)
 
 
 class RetracerMiddleware(object):
